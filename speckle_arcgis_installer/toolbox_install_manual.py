faadba8e73952f83be177a1edbb7a0fb7362385d
KatKatKateryna <kateryna@speckle.systems>
2022-11-30 02:36:46 +0800

    manual install taking the latest available 2.x whl

--- speckle_arcgis_installer/toolbox_install_manual.py
+++ speckle_arcgis_installer/toolbox_install_manual.py
@@ -4,17 +4,23 @@
 #       for 3.0.0: Project-> Package Manager-> Active Environment (Environment Manager)-> Clone arcgispro-py3
 #   2. Change the path to your new environemnt Python.exe if necessary (in variable "pythonPath" below, line 13) 
 #   3. Enter the location of 'toolbox_install_manual.py' in the following command and run this command in ArcGIS Python console (View -> Python Window)
-#   import sysconfig; import subprocess; x = sysconfig.get_paths()['data'] + r"\python.exe"; subprocess.run((x, 'C:\\Users\\myusername\\Documents\\manual_toolbox_install.py'), capture_output=True, text=True, shell=True, timeout=1000 )
+#   import sysconfig; import subprocess; x = sysconfig.get_paths()['data'] + r"\python.exe"; subprocess.run((x, 'C:\\Users\\myusername\\Documents\\toolbox_install_manual.py'), capture_output=True, text=True, shell=True, timeout=1000 )
 #   4. Restart ArcGIS Pro 
 
 from subprocess_call import subprocess_call
 import os 
+from os import listdir
+from os.path import isfile, join
 
 pythonPath = os.getenv('APPDATA').replace("\\Roaming","") + r"\Local\ESRI\conda\envs\arcgispro-py3-speckle\python.exe"
 
 def installToolbox(newExec: str):
     print("Installing Speckle Toolbox")
-    whl_file = os.path.join(os.path.dirname(__file__), "speckle_toolbox-2.9.4-py3-none-any.whl" ) 
+    mypath = os.path.dirname(__file__)
+    onlyfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f)) and "-py3-none-any.whl" in str(f))]
+    onlyfiles.sort(key = lambda x: int(x.replace("speckle_toolbox-","").replace("-py3-none-any.whl","").split(".")[1]) ) 
+    whl_file = onlyfiles[len(onlyfiles)-1]
+    #whl_file = os.path.join(os.path.dirname(__file__), "speckle_toolbox-2.9.4-py3-none-any.whl" ) 
     subprocess_call([newExec, '-m','pip','install','--upgrade', '--force-reinstall', whl_file])
     return
 
