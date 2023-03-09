2071515db0beb3c5fd6720c87642008675db24ad
KatKatKateryna <kateryna@speckle.systems>
2023-03-09 16:54:28 +0000

    geometry try/except

--- speckle_toolbox/esri/toolboxes/speckle/converter/geometry/point.py
+++ speckle_toolbox/esri/toolboxes/speckle/converter/geometry/point.py
@@ -3,24 +3,27 @@ from typing import List
 from specklepy.objects.geometry import Point
 import arcpy
 
-try: from speckle.converter.layers.utils import get_scale_factor
-except: from speckle_toolbox.esri.toolboxes.speckle.converter.layers.utils import get_scale_factor
+try: 
+    from speckle.converter.layers.utils import get_scale_factor
+    from speckle.plugin_utils.logger import logToUser
+except: 
+    from speckle_toolbox.esri.toolboxes.speckle.converter.layers.utils import get_scale_factor
+    from speckle_toolbox.esri.toolboxes.speckle.plugin_utils.logger import logToUser
 
 
 def multiPointToSpeckle(geom, feature, layer, multiType: bool):
     """Converts a Point to Speckle"""
-    #try: 
-    #print("___Point to Speckle____")
-    #point = Point(units = "m")
+    
     pointList = []
     #print(geom) # <geoprocessing describe geometry object object at 0x0000020F1D94AB10>
-    #print(multiType)
-
-    if multiType is False: 
-        for pt in geom:
-            #print(pt) # 284394.58100903 5710688.11602606 NaN NaN <class 'arcpy.arcobjects.arcobjects.Point'> 
-            #print(type(pt))
-            if pt != None: pointList.append(pointToSpeckle(pt, feature, layer)) 
+    try:
+        if multiType is False: 
+            for pt in geom:
+                #print(pt) # 284394.58100903 5710688.11602606 NaN NaN <class 'arcpy.arcobjects.arcobjects.Point'> 
+                #print(type(pt))
+                if pt != None: pointList.append(pointToSpeckle(pt, feature, layer)) 
+    except Exception as e:
+        logToUser(e)
     return pointList
 
 def pointToSpeckle(pt, feature, layer):
@@ -30,48 +33,67 @@ def pointToSpeckle(pt, feature, layer):
     # when unset, z() returns "nan"
     #print(pt) # 4.9046319 52.3592043 NaN NaN
     #print("____Point to Speckle___")
-    x = pt.X
-    y = pt.Y
-    if pt.Z: z = pt.Z 
-    else: z = 0
-    specklePoint = Point(units = "m")
-    specklePoint.x = x
-    specklePoint.y = y
-    specklePoint.z = z
-    '''
-    if feature is not None and layer is not None: # can be if it's a point from raster layer 
-        col = featureColorfromNativeRenderer(feature, layer)
-        specklePoint['displayStyle'] = {}
-        specklePoint['displayStyle']['color'] = col
-    '''
-    #print(specklePoint)
-    return specklePoint
+    try:
+        x = pt.X
+        y = pt.Y
+        if pt.Z: z = pt.Z 
+        else: z = 0
+        specklePoint = Point(units = "m")
+        specklePoint.x = x
+        specklePoint.y = y
+        specklePoint.z = z
+        '''
+        if feature is not None and layer is not None: # can be if it's a point from raster layer 
+            col = featureColorfromNativeRenderer(feature, layer)
+            specklePoint['displayStyle'] = {}
+            specklePoint['displayStyle']['color'] = col
+        '''
+        #print(specklePoint)
+        return specklePoint
+    except Exception as e:
+        logToUser(e)
+        return None
 
 def pointToNative(pt: Point, sr: arcpy.SpatialReference) -> arcpy.PointGeometry:
     """Converts a Speckle Point to QgsPoint"""
-    #print("___pointToNative__")
-    #print(pt)
-    pt = scalePointToNative(pt, pt.units)
-    geom = arcpy.PointGeometry(arcpy.Point(pt.x, pt.y, pt.z), sr, has_z = True)
-    #print(geom)
-    return geom
+    try:
+        pt = scalePointToNative(pt, pt.units)
+        geom = arcpy.PointGeometry(arcpy.Point(pt.x, pt.y, pt.z), sr, has_z = True)
+        #print(geom)
+        return geom
+    
+    except Exception as e:
+        logToUser(e)
+        return None
 
 def pointToCoord(point: Point) -> List[float]:
     """Converts a Speckle Point to QgsPoint"""
-    pt = scalePointToNative(point, point.units)
-    coords = [pt.x, pt.y, pt.z]
-    #print(coords)
-    return coords
+    try:
+        pt = scalePointToNative(point, point.units)
+        coords = [pt.x, pt.y, pt.z]
+        #print(coords)
+        return coords
+    except Exception as e:
+        logToUser(e)
+    return [None, None, None]
 
 def scalePointToNative(point: Point, units: str) -> Point:
     """Scale point coordinates to meters"""
-    scaleFactor = get_scale_factor(units)
-    pt = Point(units = "m")
-    pt.x = point.x * scaleFactor
-    pt.y = point.y * scaleFactor
-    pt.z = 0 if math.isnan(point.z) else point.z * scaleFactor
-    return pt
+    try:
+        scaleFactor = get_scale_factor(units)
+        pt = Point(units = "m")
+        pt.x = point.x * scaleFactor
+        pt.y = point.y * scaleFactor
+        pt.z = 0 if math.isnan(point.z) else point.z * scaleFactor
+        return pt
+    except Exception as e:
+        logToUser(e)
+        return None
 
 def addZtoPoint(coords: List): 
-    if len(coords) == 2: coords.append(0)
-    return coords
+    try:
+        if len(coords) == 2: coords.append(0)
+        return coords
+    except Exception as e:
+        logToUser(e)
+        return None 
--- speckle_toolbox/esri/toolboxes/speckle/converter/geometry/utils.py
+++ speckle_toolbox/esri/toolboxes/speckle/converter/geometry/utils.py
@@ -5,20 +5,25 @@ from typing import List, Union
 
 try:
     from speckle.converter.geometry.polyline import speckleArcCircleToPoints, specklePolycurveToPoints
+    from speckle.plugin_utils.logger import logToUser
 except:
     from speckle_toolbox.esri.toolboxes.speckle.converter.geometry.polyline import speckleArcCircleToPoints, specklePolycurveToPoints
+    from speckle_toolbox.esri.toolboxes.speckle.plugin_utils.logger import logToUser
 
 
 def speckleBoundaryToSpecklePts(boundary: Union[None, Polyline, Arc, Line, Polycurve]) -> List[Point]:
     #print("__speckleBoundaryToSpecklePts__")
     # add boundary points
     polyBorder = []
-    if isinstance(boundary, Circle) or isinstance(boundary, Arc): 
-        polyBorder = speckleArcCircleToPoints(boundary) 
-    elif isinstance(boundary, Polycurve): 
-        polyBorder = specklePolycurveToPoints(boundary) 
-    elif isinstance(boundary, Line): pass
-    else: 
-        try: polyBorder = boundary.as_points()
-        except: pass # if Line or None
+    try:
+        if isinstance(boundary, Circle) or isinstance(boundary, Arc): 
+            polyBorder = speckleArcCircleToPoints(boundary) 
+        elif isinstance(boundary, Polycurve): 
+            polyBorder = specklePolycurveToPoints(boundary) 
+        elif isinstance(boundary, Line): pass
+        else: 
+            try: polyBorder = boundary.as_points()
+            except: pass # if Line or None
+    except Exception as e:
+        logToUser(e)
     return polyBorder
